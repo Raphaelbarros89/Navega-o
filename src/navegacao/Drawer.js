4bbde2e3b2f790740ab7d4b398a6002201c5a287
Raphael Ricardo de Souza Barros <raphaelbarros89@gmail.com>
2023-02-16 13:13:34 -0300

    .

--- src/navegacao/Drawer.js
+++ src/navegacao/Drawer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createAppContainer, createDrawerNavigator } from '@react-navigation/drawer'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
@@ -7,7 +7,7 @@ import TelaC from '../views/TelaC'
 const Drawer = createDrawerNavigator()
 
 export default props => (
-    <Drawer.Navigator initialRouteName="TelaD">
+    <Drawer.Navigator initialRouteName="TelaB">
         <Drawer.Screen name="TelaA" component={TelaA} />
         <Drawer.Screen name="TelaB" component={TelaB} />
         <Drawer.Screen name="TelaC" component={TelaC} />
